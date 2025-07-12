6735df2a711f3883dfe470857e09b1596771fe3e
Zaid Ashraf <zaidashraf249@gmail.com>
2025-07-12 10:18:56 +0530

    Second Commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,9 @@
-import { clsx } from "clsx"
+import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 /**
  * Merge Tailwind classes with conflict resolution
  */
-export function cn(...inputs: any[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
